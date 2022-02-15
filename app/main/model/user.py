1167e5d6dbca39c30d628db3aca2a23cb5bd90b0
ang67 <binikouadiojosias@gmail.com>
2022-02-15 22:42:23 +0100

    Add model for migration + unit test

---
+++ app/main/model/user.py
@@ -0,0 +1,31 @@
+from .. import db, flask_bcrypt
+
+class User(db.Model):
+    """ User Model for storing user related details """
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    firstname = db.Column(db.String(255), nullable=False)
+    lastname = db.Column(db.String(255), nullable=False)
+    date_of_birth = db.Column(db.DateTime, nullable=False)
+    bio = db.Column(db.String(800))
+    photo_ref = db.Column(db.String(255))
+    docs_ref = db.Column(db.String(255))
+    id_checked = db.Column(db.Boolean, nullable=False, default=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    password_hash = db.Column(db.String(100))
+
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
